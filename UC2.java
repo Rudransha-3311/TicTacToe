2de71c53f0dc97a4d6c4f03e7dde244b7c4af6f6
Rudransha-3311 <rr6972@srmist.edu.in>
2026-04-21 14:42:05 +0530

    Add UC2 implem

---
+++ UC2.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+/**
+ * UC2 performs a random toss to decide who plays first and assigns
+ * symbols (X or O) to the human and computer accordingly.
+ */
+public class UC2 {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and will play first.");
+        } else {
+            System.out.println("Computer won the toss and will play first.");
+        }
+
+        System.out.println("Human symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
+    }
+}
